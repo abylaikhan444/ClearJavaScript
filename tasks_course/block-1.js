0540dc29a01784e827d58448d656687ad1b54a62
abylaikhan444 <fifa77882@gmail.com>
2026-03-20 03:05:53 +0500

    block 1 tasks

---
+++ tasks_course/block-1.js
@@ -0,0 +1,15 @@
+// TASK 1
+// let arr = [3, 7, 2, 9, 5];
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//             [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//         }
+//     }
+// }
+//
+// Last item is big sum, maybe
+// console.log(arr[arr.length - 1]);
+
+
+//
