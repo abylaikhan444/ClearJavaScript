5dd8d4eaf79a92421834cb42aafcbfd4c4caedc6
abylaikhan444 <fifa77882@gmail.com>
2026-03-20 21:31:21 +0500

    block 1 finished

---
+++ tasks_course/block-1(array).js
@@ -0,0 +1,55 @@
+// TASK 1
+// let arr = [3, 7, 2, 9, 5];
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//             [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//         }
+//     }
+// }
+//
+// Last item is big sum, maybe its not good method for the find max num)
+// console.log(arr[arr.length - 1]);
+
+
+// TASK 2
+
+// let arr = [1, 2, 3, 4, 5];
+// let resultSum = 0;
+//
+// for (let i = 0; i < arr.length; i++) {
+//     resultSum += arr[i];
+// }
+//
+// console.log(resultSum);
+
+
+// TASK 3
+// создать новый массив только с четными числами!
+
+// let arr = [];
+//
+// for (let i = 1; i < 50; i++) {
+//     if (i % 2 === 0) {
+//         arr.push(i);
+//     }
+// }
+//
+// console.log(arr);
+
+
+// TASK 4
+// проверить, есть ли "admin"
+
+// let users = ["admin", "user", "test"];
+// let result = '';
+// for (let i = 0; i < users.length; i++) {
+//     if (users[i] === "admin") {
+//         result = 'TRUE';
+//     } else {
+//         result = 'FALSE';
+//     }
+// }
+//
+// console.log(result);
+
